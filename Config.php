34e7029e1fe0fa50c6b0977ea6820ce6e4865593
srikanthchary94 <manchanpallysrikanth82@gmail.com>
2025-04-22 10:00:04 +0530

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'onedaoacademy.postgres.database.azure.com';
+$CFG->dbname    = 'postgres';
+$CFG->dbuser    = 'onedao';
+$CFG->dbpass    = 'Mnbvcxzasdf@';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 5432,
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'https://oda.onedao.me';
+$CFG->sslproxy  =  true;
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
